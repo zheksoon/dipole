d993e5ebc828c42e1a166ae3a0c07585619d006b
Eugene Daragan <zheksoon@gmail.com>
2020-10-29 21:24:28 +0300

    HashSet: sacrify inplace rehashing for better readability

--- src/utils/hash-set.js
+++ src/utils/hash-set.js
@@ -30,7 +30,7 @@ class HashSet {
             items[hash] = item;  
 
             if (++this._size > (length * FILL_FACTOR_BY_16) >> 4) {
-                this._rehashUp(length << 1);
+                this._rehashUp(length * 2);
             }
             return true;
         }
@@ -43,48 +43,21 @@ class HashSet {
     * @param length {int} - new hash table length, should be power of 2
     */
     _rehashUp(length) {
-        const items = this._items;
-        const modulo = length - 1;
-        const oldLength = items.length;
+        const oldItems = this._items;
+        const oldLength = oldItems.length;
 
-        // resize the table and fill it with 'undefined' - gives performance boost for V8
-        items.length = length;
-        for (let i = oldLength; i < length; i++) {
-            items[i] = undefined;
-        }
-
-        // doubling the length of the table reveals one more bit in item's hash
-        // so each item may stay in the same half of the table or move to another one.
-        // reinserting into another (empty) half of hash table doesn't generate any special cases,
-        // but reinserting into the same half of hash table has some special cases.
-        // let's imagine a very simple hash table:
-        // [ # 1 # 7 ], where # designates empty cell, and a number is an element's hash.
-        // and try to insert, say, element with hash 3:
-        // [ 3 1 # 7 ]. (it was aliased with 7 and wrapped to the first empty cell after it)
-        // when rehashing this table up (doubling its size), at some moment the 3 will be inserted 
-        // after the 7 because we still haven't reinserted the 7 (only the 3 was processed):
-        // [ # 1 # 7 3 # # # ]
-        // so if we process only the lower half of the table
-        // the result will be invalid hash table (the 3 can't be found on its place):
-        // [ # 1 # # 3 # # 7 ].
-        // to avoid this, we need to process an extra probing sequence after the first half
-        // to move elements to their correct places:
-        // [ # 1 # 3 # # # 7 ].
-        // this is as simple as 'continue; else break;' statement lower, but it took few days to realize it :)
-        // it can be proven that only the extra sequence can contain elements with invalid positions.
+        const newItems = new Array(length);
         for (let i = 0; i < length; i++) {
-            const item = items[i];
-            if (item === undefined) {
-                if (i < oldLength) continue; else break;
-            }
-            items[i] = undefined;
+            newItems[i] = undefined;
+        }
+        this._items = newItems;
+        this._size = 0;
 
-            let hash = item._hash & modulo;
-            while (items[hash] !== undefined) {
-                hash = (hash + 1) & modulo;
+        for (let i = 0; i < oldLength; i++) {
+            const item = oldItems[i];
+            if (item !== undefined) {
+                this.add(item)
             }
-
-            items[hash] = item;
         }
     }
 
