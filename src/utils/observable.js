77234b0861b03d5e2615fbebccab8b7d41077faf
Eugene Daragan <zheksoon@gmail.com>
2021-05-04 23:25:51 +0300

    prettier

--- src/utils/observable.js
+++ src/utils/observable.js
@@ -1,12 +1,12 @@
-import { Observable, Computed } from '../dipole'
+import { Observable, Computed } from "../dipole";
 
 export function makeObservable(obj) {
     const descriptors = [];
     for (const key in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            const currentDescriptor = Object.getOwnPropertyDescriptor(obj, key)
+            const currentDescriptor = Object.getOwnPropertyDescriptor(obj, key);
             if (currentDescriptor.configurable && currentDescriptor.value !== undefined) {
-                const prop = currentDescriptor.value
+                const prop = currentDescriptor.value;
 
                 if (prop instanceof Observable) {
                     descriptors.push({
@@ -14,28 +14,28 @@ export function makeObservable(obj) {
                         enumerable: true,
                         configurable: true,
                         get() {
-                            return prop.get()
+                            return prop.get();
                         },
                         set(value) {
-                            prop.set(value)
+                            prop.set(value);
                         },
-                    })
+                    });
                 } else if (prop instanceof Computed) {
                     descriptors.push({
                         key: key,
                         enumerable: true,
                         configurable: true,
                         get() {
-                            return prop.get()
+                            return prop.get();
                         },
-                    })
+                    });
                 }
             }
         }
     }
     for (let i = 0; i < descriptors.length; i++) {
-        const descriptor = descriptors[i]
-        Object.defineProperty(obj, descriptor.key, descriptor)
+        const descriptor = descriptors[i];
+        Object.defineProperty(obj, descriptor.key, descriptor);
     }
-    return obj
+    return obj;
 }
