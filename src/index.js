fc4d2d6be0117bc8cb209ac41949359067e08a67
Eugene Daragan <zheksoon@gmail.com>
2021-11-04 00:35:58 +0300

    Deployed b0a44ca with MkDocs version: 1.2.3

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./core/classes";
-
-export { tx, utx, untracked, action } from "./core/transaction";
-export { fromGetter, notify } from "./core/extras";
-
-export * from "./utils/observable";
-export * from "./utils/reaction";
